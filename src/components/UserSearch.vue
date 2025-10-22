46f7dc1c20c637894d22380bd3fa32e67a3452c3
DavidHan663 <dhan27848@gmail.com>
2025-10-22 22:40:32 +0800

    优化搜索结果显示

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -1,20 +1,45 @@
 <template>
     <div>
         <input v-model="username" placeholder="输入用户名"/>
-        <button @click="test">测试输入</button>
+        <button @click="searchUsers">搜素</button>
+        <p v-if="loading">加载中...</p>
+        <p v-else-if="error">{{ error }}</p>
+        <p v-else-if="users.length>0">找到{{ users.length }}个用户</p>
+        <p v-else-if="!loading&&!error">无结果</p>
+        <ul v-if="users.length>0">
+            <li v-for="user in users" :key="user.id">
+                <img :src="user.avatar_url" width="50" />
+                {{ user.login }}(ID:{{ user.id }})
+            </li>
+        </ul>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default{
-    name:"UserSearch",
+    name:'UserSearch',
     data(){
         return{
-            username:'octocat'
+            username:'',
+            users:[],
+            loading:false,
+            error:null
         };
     },
     methods:{
-        test(){
-            alert(`输入用户名为${this.username}`);
+        async searchUsers(){
+            this.loading=true;
+            this.error=null;
+            try{
+                const response=await axios.get(
+                    `https://api.github.com/search/users?q=${this.username}+in:login`
+                );
+                this.users=response.data.items;
+            }catch(err){
+                this.error='搜索失败，稍后重试';
+            }finally{
+                this.loading=false;
+            }
         }
     }
 };
@@ -32,5 +57,9 @@ button {
   background-color: #0066cc;
   color: white;
   border: none;
+}
+img {
+  vertical-align: middle;
+  margin-right: 10px;
 }
 </style>
