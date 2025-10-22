4a8aed6b358d47b53a88d95949abd4c87a65003d
DavidHan663 <dhan27848@gmail.com>
2025-10-22 18:54:36 +0800

    初始化 Vue 项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Github 用户搜索</h1>
+    <p>我的第一个Vue 项目</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  
+}
+</script>
+
+<style>
+h1{
+  color:blue;
+  text-align:center;
+}
+p{
+  text-align:center;
+}
+</style>
