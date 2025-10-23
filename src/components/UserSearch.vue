1f6d10d691f693cd551e79beb59d96aea3da52c8
DavidHan663 <dhan27848@gmail.com>
2025-10-23 21:06:21 +0800

    优化Element plus布局和书签功能

--- src/components/UserSearch.vue
+++ src/components/UserSearch.vue
@@ -1,65 +1,139 @@
 <template>
-    <div>
-        <input v-model="username" placeholder="输入用户名"/>
-        <button @click="searchUsers">搜素</button>
-        <p v-if="loading">加载中...</p>
-        <p v-else-if="error">{{ error }}</p>
-        <p v-else-if="users.length>0">找到{{ users.length }}个用户</p>
-        <p v-else-if="!loading&&!error">无结果</p>
-        <ul v-if="users.length>0">
-            <li v-for="user in users" :key="user.id">
-                <img :src="user.avatar_url" width="50" />
-                {{ user.login }}(ID:{{ user.id }})
-            </li>
-        </ul>
-    </div>
+  <div class="container">
+    <el-input
+      v-model="username"
+      placeholder="输入 GitHub 用户名"
+      clearable
+      class="search-input"
+    />
+    <el-button type="primary" @click="searchUsers">搜索</el-button>
+    <el-alert v-if="loading" title="加载中..." type="info" :closable="false" />
+    <el-alert v-else-if="error" :title="error" type="error" :closable="false" />
+    <el-alert
+      v-else-if="users.length > 0"
+      :title="`找到 ${users.length} 个用户`"
+      type="success"
+      :closable="false"
+    />
+    <el-alert
+      v-else-if="!loading && !error"
+      title="无结果"
+      type="warning"
+      :closable="false"
+    />
+    <el-row v-if="users.length > 0" :gutter="20">
+      <el-col
+        :span="8"
+        v-for="user in users"
+        :key="user.id"
+        class="user-card"
+      >
+        <el-card shadow="hover">
+          <img :src="user.avatar_url" width="50" />
+          <span>{{ user.login }}</span>
+          <el-button
+            type="warning"
+            size="small"
+            @click="toggleBookmark(user)"
+          >
+            <el-icon><StarFilled /></el-icon>
+            {{ isBookmarked(user) ? '移除书签' : '添加书签' }}
+          </el-button>
+        </el-card>
+      </el-col>
+    </el-row>
+    <h3 v-if="bookmarks.length > 0">我的书签</h3>
+    <el-row v-if="bookmarks.length > 0" :gutter="20">
+      <el-col
+        :span="8"
+        v-for="bookmark in bookmarks"
+        :key="bookmark.id"
+        class="user-card"
+      >
+        <el-card shadow="hover">
+          <img :src="bookmark.avatar_url" width="50" />
+          <span>{{ bookmark.login }}</span>
+          <el-button
+            type="danger"
+            size="small"
+            @click="removeBookmark(bookmark)"
+          >
+            <el-icon><Delete /></el-icon>
+            删除
+          </el-button>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
 import axios from 'axios';
-export default{
-    name:'UserSearch',
-    data(){
-        return{
-            username:'',
-            users:[],
-            loading:false,
-            error:null
-        };
+import { StarFilled, Delete } from '@element-plus/icons-vue';
+export default {
+  name: 'UserSearch',
+  components: { StarFilled, Delete },
+  data() {
+    return {
+      username: '',
+      users: [],
+      loading: false,
+      error: null,
+      bookmarks: JSON.parse(localStorage.getItem('bookmarks')) || []
+    };
+  },
+  methods: {
+    async searchUsers() {
+      this.loading = true;
+      this.error = null;
+      this.users = [];
+      try {
+        const response = await axios.get(
+          `https://api.github.com/search/users?q=${this.username}`
+        );
+        this.users = response.data.items;
+      } catch (err) {
+        this.error = '搜索失败，请稍后重试';
+      } finally {
+        this.loading = false;
+      }
     },
-    methods:{
-        async searchUsers(){
-            this.loading=true;
-            this.error=null;
-            try{
-                const response=await axios.get(
-                    `https://api.github.com/search/users?q=${this.username}+in:login`
-                );
-                this.users=response.data.items;
-            }catch(err){
-                this.error='搜索失败，稍后重试';
-            }finally{
-                this.loading=false;
-            }
-        }
+    toggleBookmark(user) {
+      const index = this.bookmarks.findIndex(b => b.id === user.id);
+      if (index === -1) {
+        this.bookmarks.push({ id: user.id, login: user.login, avatar_url: user.avatar_url });
+      } else {
+        this.bookmarks.splice(index, 1);
+      }
+      localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
+    },
+    isBookmarked(user) {
+      return this.bookmarks.some(b => b.id === user.id);
+    },
+    removeBookmark(bookmark) {
+      this.bookmarks = this.bookmarks.filter(b => b.id !== bookmark.id);
+      localStorage.setItem('bookmarks', JSON.stringify(this.bookmarks));
     }
+  }
 };
-
-
 </script>
-
 <style scoped>
-input {
-  padding: 8px;
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+.search-input {
+  width: 300px;
   margin-right: 10px;
 }
-button {
-  padding: 8px 16px;
-  background-color: #0066cc;
-  color: white;
-  border: none;
+.user-card {
+  margin-bottom: 20px;
 }
 img {
   vertical-align: middle;
   margin-right: 10px;
+}
+h3 {
+  margin-top: 20px;
 }
 </style>
