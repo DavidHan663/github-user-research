11f3d103bd7e02ccb2cc21b89a41442f2901f978
DavidHan663 <dhan27848@gmail.com>
2025-10-24 10:41:59 +0800

    ServiceWorker缓存页面

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-const app=createApp(App)
+const app = createApp(App);
 app.use(ElementPlus);
-for(const[key,component] of Object.entries(ElementPlusIconsVue)){
-    app.component(key,component);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
 }
-app.mount('#app')
+app.mount('#app');
 
+// 注册 Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/dist/sw.js')
+      .then(registration => {
+        console.log('Service Worker 注册成功:', registration);
+      })
+      .catch(error => {
+        console.log('Service Worker 注册失败:', error);
+      });
+  });
+}
